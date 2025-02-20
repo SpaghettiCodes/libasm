b354e0e8a6f71c2dd75266955480c1aa89c6a687
shixiang-chia <shixiang-chia@alphacloud.com.my>
2025-02-20 17:59:07 +0800

    didnt do bonus slept instead

--- bonus/main.c
+++ bonus/main.c
@@ -6,6 +6,8 @@ typedef struct s_list
     struct s_list *next;
 }     t_list;
 
+void  ft_list_push_front(t_list **begin_list, void *data);
+
 int main()
 {
   t_list start;
@@ -16,4 +18,11 @@ int main()
   start.next = &next;
 
   printf("%s\n", (char *) start.data);
+  printf("%p\n", &start);
+  printf("%p\n", &start.data);
+  printf("%p\n", &start.next);
+  printf("%p\n", &next);
+  printf("%p\n", &next.data);
+  printf("%p\n", &next.next);
+
 }
