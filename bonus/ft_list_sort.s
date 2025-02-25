831cb1b2457548adba0465d595acdbcc758dcbca
SpaghettiCodes <chuashixiang@gmail.com>
2025-02-25 23:33:08 +0800

    well thats awkward

---
+++ bonus/ft_list_sort.s
@@ -0,0 +1,12 @@
+global ft_list_sort
+
+; void	ft_list_sort(t_list **begin_list, int (*cmp)())
+
+ft_list_sort:
+  enter 0, 0
+
+end:
+  leave
+  ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
