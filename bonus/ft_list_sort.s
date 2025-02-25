5279f06b29c71dc6ab73ace0919d5fe7b6fd381a
SpaghettiCodes <chuashixiang@gmail.com>
2025-02-25 23:43:14 +0800

    did abit

--- bonus/ft_list_sort.s
+++ bonus/ft_list_sort.s
@@ -1,9 +1,29 @@
 global ft_list_sort
 
 ; void	ft_list_sort(t_list **begin_list, int (*cmp)())
+; begin list = rdi
+; cmp rsi
+; rdx cur
+; rcx cur2
+; r8 prev2
 
 ft_list_sort:
   enter 0, 0
+  mov r10, [rdi]
+  mov rdx, [rdi + 8]
+
+loop:
+  xor r8, r8
+  mov rcx, [rdi]
+
+innerloop:
+  // check stuff here
+
+  mov r8, rcx
+  mov rcx, [rcx + 8]
+
+  mov rdx, [rdx + 8]
+  jmp loop
 
 end:
   leave
